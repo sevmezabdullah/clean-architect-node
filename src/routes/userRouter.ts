e9dc605f35fd0ebe069a530c216810d24556cb27
sevmezabdullah <abdullah.sevmez@rapid.com.tr>
2024-02-11 13:46:53 +0300

    controller done

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -5,6 +5,7 @@ import { INTERFACE_TYPE } from '../utils';
 import { UserInteractor } from '../interactors/userInteractor';
 import { IUserRepository } from '../interfaces/user/IUserRepository';
 import { UserRepository } from '../repositories/userRepository';
+import { UserController } from '../controllers/userController';
 
 const userRouter = express.Router()
 
@@ -13,5 +14,8 @@ const container = new Container()
 
 container.bind<IUserInteractor>(INTERFACE_TYPE.UserInteractor).to(UserInteractor)
 container.bind<IUserRepository>(INTERFACE_TYPE.UserRepository).to(UserRepository)
+container.bind(INTERFACE_TYPE.UserController).to(UserController)
+
+const controller = container.get<UserController>(INTERFACE_TYPE.UserController)
 
 export default userRouter;
--- src/utils/appConsts.ts
+++ src/utils/appConsts.ts
@@ -1,6 +1,8 @@
-import { UserInteractor } from "../interactors/userInteractor";
+
+
 
 export const INTERFACE_TYPE = {
     UserInteractor: Symbol.for("IUserInteractor"),
-    UserRepository: Symbol.for("IUserRepository")
+    UserRepository: Symbol.for("IUserRepository"),
+    UserController: Symbol.for('UserController')
 }
